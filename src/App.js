3cbb96f0c11f5144eedef9f2b39565929b15c006
jtobin321 <jtobin321@gmail.com>
2019-12-09 16:03:56 -0500

    added a bunch of stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Grid, Form, Header, Segment, Button, Dropdown, Message } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default class extends Component {
+  state = {
+    n: "",
+    e: "",
+    text: "",
+    type: "",
+    errors: false
+  }
+
+  nChangeHandler = (e, value) => {
+      this.setState({
+          n: value.value
+      })
+  }
+
+  eChangeHandler = (e, value) => {
+      this.setState({
+          e: value.value
+      })
+  }
+
+  textChangeHandler = (e, value) => {
+    this.setState({
+        text: value.value
+    })
+  }
+
+  typeChangeHandler = (e, value) => {
+    this.setState({
+        type: value.value
+    })
+  }
+
+  submitHandler = () => {
+      console.log(this.state)
+      if (this.state.n == "" || this.state.e == "" || this.state.type == "" || this.state.text == "") {
+          this.setState({
+              errors: true
+          })
+      }
+      else {
+        this.setState({
+            errors: false
+        })
+        //other
+      }
+      
+  }
+
+
+  render() {
+    return (
+      <div>
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 700 }}>
+                    <Header as='h1' color='teal' textAlign='center'>RSA Encryption</Header>
+                    <Form size='large'>
+                        <Segment stacked>
+                        <Message negative visible={this.state.errors} hidden={!this.state.errors}>
+                            <Message.Header>There are errors with this form</Message.Header>
+                            <p>Please make sure all of the fields are filled out properly</p>
+                        </Message>
+                            <Form.Group widths='equal'>
+                                <Form.Input fluid onChange={this.nChangeHandler} label="Choose your N:" placeholder="N = p * q where p, q are prime" />
+                                <Form.Input fluid onChange={this.eChangeHandler} label="Choose your e:" placeholder="gcd(e, (p-1) * (q-1)) = 1" /> 
+                                <Dropdown 
+                                    fluid
+                                    selection
+                                    label="Encrypt or Decrypt" 
+                                    placeholder='Encrypt or Decrypt' 
+                                    options={[{key: 'en', value: 'en', text: 'Encrypt'}, {key: 'de', value: 'de', text: 'Decrypt'}]} 
+                                    onChange={this.typeChangeHandler}    
+                                />
+                            </Form.Group>
+                            <Form.Group widths='equal'>
+                                <Form.Input fluid onChange={this.textChangeHandler} type={this.state.type == "en" ? 'password' : 'text'} label= "Enter text" placeholder="Enter text..." />
+                            </Form.Group>
+                            <Form.Group widths='equal'>
+                                <Button color='teal' fluid onClick={this.submitHandler}>Submit</Button>
+                            </Form.Group>
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+            </Grid>
+      </div>
+    )
+  }
 }
-
-export default App;
